e752e62398b1e315d85f3ae9489e2d98557e94d9
Casimir Pohjanraito <casimir.pohjanraito@gmail.com>
2022-07-21 20:24:10 +0300

    Rule movement cost defaults.

--- src/ruleset/rule-calculator.rb
+++ src/ruleset/rule-calculator.rb
@@ -7,18 +7,25 @@ limits = {
   # • Time-limit vs Movement Turns: Number/time of moves required to visit of 33% of Sectors
   # DD 4.9.1. 12 minute time limit
   :time_max_seconds => 720,
-  # DD 4.1.1. 33% of sectors
+  # DD 4.1.1. able to visit 33% of sectors
   :sectors_visited_min => 0.33,
 
   # • Sector size: the game board size, the world grid resolution.
   # DD 3.2.2. 40x40 game board
-  :default_sector_grid => 40
+  :sector_grid_default => 40
+}
 
-  # - minimum time/move limit required for World @ Sector size
+defaults = {
+  # DD 4.1.
+  # • Movement cost: negative multiplier from elevation (unit_v - elev_km * multi)
+  :movement_elevation_multiplier_per_km => 66,
+
+  # • Polar latitude cost: reduction multiplier (unit_v * multi * min(0, (deg-tres)))
+  :movement_reduction_latitude_treshold => 60,
+  :movement_reduction_latitude_per_degree => 2,
 
   # • Lidar resolution: the rendering elevation interval, and polygonal resolution, to ensure clear visuals.
-  # • Movement cost: negative multiplier from elevation and terrain (ie. mountains limit movement).
-  # • Polar latitude cost: negative multiplier from declination (ant/arctic is hard).
+  :reso_lidar_elev_m => 5
 }
 
 # Game instance parameters
@@ -28,14 +35,15 @@ Vote = Struct.new(:last_powerstructure_bool, :head_of_chiefs_bool, :fast_mode_bo
 
 # In-game-object mocks
 World = Struct.new(:size, :equatorial_circumference_km)
-Unit = Struct.new(:type, :society, :maximum_velocity)
+Unit = Struct.new(:type, :society, :work_hours, :maximum_velocity)
 
 # Instance mocks
-world_small = World.new(40, 10000);
-world_medium = World.new(160, 40000);
-world_large = World.new(640, 120000);
+world_small = World.new(40, 10000)
+world_medium = World.new(160, 40000)
+world_large = World.new(640, 120000)
 
-players_head = Unit.new("Head", nil, 140)
+# DD 4.3.
+players_head = Unit.new("Head", nil, 8, 140)
 
 #pseudo game logic to mock resultant gameplay
 #f.ex. mock_gameplay(buildingRandomizer)
@@ -44,6 +52,7 @@ players_head = Unit.new("Head", nil, 140)
 
 #ESTIMATE SCORE:
 #  optimal sector size (game world resolution)
+#  - minimum time/move limit required for World @ Sector size
 #  memory & cpu req
 #  rule sanity score
 
