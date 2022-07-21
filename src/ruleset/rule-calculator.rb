d74fdc13d7f114fd3680639869bb83b2b2c26394
Casimir Pohjanraito <casimir.pohjanraito@gmail.com>
2022-07-21 23:41:49 +0300

    Add DD 4.1.1. estimate.

--- src/ruleset/rule-calculator.rb
+++ src/ruleset/rule-calculator.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# RULE CALCULATOR
+puts 'RULE CALCULATOR'
 
 # Design Doc chapter parameters
 limits = {
@@ -10,17 +10,18 @@ limits = {
   # DD 4.1.1. able to visit 33% of sectors
   :sectors_visited_min => 0.33,
 
-  # • Sector size: the game board size, the world grid resolution.
-  # DD 3.2.2. 40x40 game board
-  :sector_grid_default => 40
 }
 
 defaults = {
+  # • Sector size: the game board size, the world grid resolution.
+  # DD 3.2.2. 40x40 game board
+  :sector_grid => 40,
+
   # DD 4.1.
   # • Movement cost: negative multiplier from elevation (unit_v - elev_km * multi)
   :movement_elevation_multiplier_per_km => 66,
 
-  # • Polar latitude cost: reduction multiplier (unit_v * multi * min(0, (deg-tres)))
+  # • Polar latitude cost: reduction multiplier unit_v * (multi * min(0, (deg-tres)))
   :movement_reduction_latitude_treshold => 60,
   :movement_reduction_latitude_per_degree => 2,
 
@@ -34,8 +35,8 @@ Ruleset = Struct.new(:turn_mode, :turn_lenght, :session_lenght)
 Vote = Struct.new(:last_powerstructure_bool, :head_of_chiefs_bool, :fast_mode_bool, :beginner_mode_bool, :turn_per_day_bool)
 
 # In-game-object mocks
-World = Struct.new(:size, :equatorial_circumference_km)
-Unit = Struct.new(:type, :society, :work_hours, :maximum_velocity)
+World = Struct.new(:size, :circumference_km)
+Unit = Struct.new(:type, :society, :work_hours, :velocity_kmh)
 
 # Instance mocks
 world_small = World.new(40, 10000)
@@ -43,16 +44,26 @@ world_medium = World.new(160, 40000)
 world_large = World.new(640, 120000)
 
 # DD 4.3.
-players_head = Unit.new("Head", nil, 8, 140)
+head = Unit.new("Head", nil, 14, 140)
+
+puts "RESULTS"
+puts "DD 4.1.1. - time/moves required for World @ Sector size"
+sector_side_km = world_medium[:circumference_km]/defaults[:sector_grid]
+sector_visit_min = (defaults[:sector_grid]**2 * limits[:sectors_visited_min])
+travel_min_km = sector_side_km * sector_visit_min 
+travel_time_min_h = travel_min_km / (head[:work_hours] * head[:velocity_kmh])
+puts "Minimum to travel #{sector_visit_min.to_int} sectors:"
+puts "#{travel_time_min_h.to_int} hours (optimal, no elevation/latitude multi)"
 
 #pseudo game logic to mock resultant gameplay
 #f.ex. mock_gameplay(buildingRandomizer)
 #      Society.new(players_head, building)
 #      ...
 
+
+#
 #ESTIMATE SCORE:
 #  optimal sector size (game world resolution)
-#  - minimum time/move limit required for World @ Sector size
 #  memory & cpu req
 #  rule sanity score
 
