c2c2a56ae1623d4a0c62701dd2ab4f6659997a21
Casimir Pohjanraito <casimir.pohjanraito@gmail.com>
2022-07-29 04:48:39 +0300

    Update rules.

--- src/ruleset/rule-calculator.rb
+++ src/ruleset/rule-calculator.rb
@@ -6,7 +6,11 @@ puts 'RULE CALCULATOR'
 limits = {
   # • Time-limit vs Movement Turns: Number/time of moves required to visit of 33% of Sectors
   # DD 4.9.1. 12 minute time limit
-  :time_max_seconds => 720,
+  :time_max_s => 720,
+  # DD 4.9.2
+  :time_turn_len_s => 17,
+  # DD 4.9.3
+  :time_days_per_turn => 17,
   # DD 4.1.1. able to visit 33% of sectors
   :sectors_visited_min => 0.33,
 
@@ -15,7 +19,7 @@ limits = {
 defaults = {
   # • Sector size: the game board size, the world grid resolution.
   # DD 3.2.2. 40x40 game board
-  :sector_grid => 40,
+  :sector_grid => 36,
 
   # DD 4.1.
   # • Movement cost: negative multiplier from elevation (unit_v - elev_km * multi)
@@ -39,33 +43,41 @@ World = Struct.new(:size, :circumference_km)
 Unit = Struct.new(:type, :society, :work_hours, :velocity_kmh)
 
 # Instance mocks
-world_small = World.new(40, 10000)
-world_medium = World.new(160, 40000)
-world_large = World.new(640, 120000)
+worlds = {
+  :small => World.new(40, 10000),
+  :medium => World.new(160, 40000),
+  :large => World.new(640, 120000)
+}
 
 # DD 4.3.
-head = Unit.new("Head", nil, 14, 140)
+head = Unit.new("Head", nil, 14, 120)
 
 puts "RESULTS"
-puts "DD 4.1.1. - time/moves required for World @ Sector size"
-sector_side_km = world_medium[:circumference_km]/defaults[:sector_grid]
-sector_visit_min = (defaults[:sector_grid]**2 * limits[:sectors_visited_min])
+puts "Iteration 1 result: sector size mapped on a World globe is a wack idea. Since the scenario is post-mini-apocalypse, and tech-tree doesn't allow fast travel, gameplay should not be based on units traveling a lot of ground. Emphasis on lidar & network capacities, charm ability and social dynamic, plus reducing entropy."
+puts "Sectors should simply represent some angle of the World globe. For arena subdivision, 18x18 might be enough, 10 degree. Further, the sectors area is smaller towards the polar caps. The DD and rule calculator should be updated to represent this."
+puts "_"
+puts "DD 4.1.1. - time/moves required for minimum sectors"
+sectors = defaults[:sector_grid]**2 
+sector_side_km = worlds[:medium][:circumference_km]/defaults[:sector_grid]
+sector_visit_min = sectors * limits[:sectors_visited_min]
 travel_min_km = sector_side_km * sector_visit_min 
-travel_time_min_h = travel_min_km / (head[:work_hours] * head[:velocity_kmh])
-puts "Minimum to travel #{sector_visit_min.to_int} sectors:"
-puts "#{travel_time_min_h.to_int} hours (optimal, no elevation/latitude multi)"
+travel_time_min_d = travel_min_km / (head[:work_hours] * head[:velocity_kmh])
+travel_time_min_turns = (travel_time_min_d/limits[:time_days_per_turn]).to_int
+puts "Head-unit time to travel #{sector_visit_min.to_int} of #{sectors} sectors (no elevation/lati multi):"
+puts "#{travel_time_min_d.to_int} days, #{travel_time_min_turns} turns, #{(limits[:time_turn_len_s]*travel_time_min_turns)/60} minutes."
+puts "_"
+puts "DD 4.9. - Temporal Mechanics"
+turns_min = limits[:time_max_s]/limits[:time_turn_len_s]
+puts "For a #{limits[:time_max_s]/60} minute game, #{turns_min*limits[:time_days_per_turn]} in-world days." 
+puts "Minimum number of turns #{turns_min}."
+
 
 #pseudo game logic to mock resultant gameplay
 #f.ex. mock_gameplay(buildingRandomizer)
 #      Society.new(players_head, building)
-#      ...
 
-
-#
 #ESTIMATE SCORE:
-#  optimal sector size (game world resolution)
+#  Unit travel score
 #  memory & cpu req
 #  rule sanity score
-
 #  game_winnable
-
