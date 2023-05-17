b13f838b48a405df5a69da16f13a922699fb38c3
Casimir Pohjanraito <casimir.pohjanraito@gmail.com>
2023-05-17 17:04:01 +0300

    Update rule calculator.

--- src/ruleset/rule-calculator.rb
+++ src/ruleset/rule-calculator.rb
@@ -5,12 +5,23 @@ os = ['RULE CALCULATOR']
 
 # Design Doc [chapter] [name]
 spec = {
-  # DD 4.1.
+  # DD 4.1.1.
   # • Lidar resolution: the rendering elevation interval, ensure clear visuals.
   :reso_lidar_elev_m => 5,
 
+  # • Entropy-effect probability: signal lost
+  # fail % for meeting call & vote, sight & lidar, object-use, directing followers
+  :entropy_global_risk_percent => 5,
+
   # • Movement cost from elevation (unit_v - elev_km * multi)
   :movement_cost_elevation_per_km => 66,
+
+  # DD 4.2.2. Solar Insolation
+  :insolation_avg_wsqm => 1000,
+
+  # DD 4.2.3. Orbital Debris Cataclysm
+  # Percent of max turns before cataclysm
+  :cataclysm_start_turns_percent => 50,
 
   # DD 4.2.4. Map sectors (10 deg lat/long).
   :sector_grid => 36,
@@ -19,6 +30,12 @@ spec = {
   # cost: reduction multiplier unit_v * (multi * min(0, (deg-tres)))
   :movement_cost_latitude_treshold => 50,
   :movement_cost_latitude_per_degree => 2,
+
+  # DD 4.3.1. Local units
+  :units_locals_per_sqkm_min => 1,
+
+  # DD 4.4.1. Buildings
+  :building_biosphere_in_kgpd_min => 4,
 
   # DD 4.9.1. 12 minute time limit
   :time_max_s => 720,
@@ -32,28 +49,30 @@ limits = {
   # DD 4.1.1. able to visit 33% of sectors
   # • Time-limit vs Movement Turns: Number/time of moves for Head unit
   :sectors_visited_min => 0.33,
+  # • Building/unit limits
+  :building_sight_max_r_km => 85
 }
 
 # Game instance parameters
-Session = Struct.new(:view, :players, :rule_set, :turn)
-Ruleset = Struct.new(:turn_mode, :turn_lenght, :session_lenght)
 Vote = Struct.new(:last_powerstructure_bool,
                   :head_of_chiefs_bool,
                   :fast_mode_bool,
                   :beginner_mode_bool,
                   :turn_per_day_bool)
+Ruleset = Struct.new(:turn_mode, :turn_lenght, :session_lenght)
+Session = Struct.new(:view, :players, :rule_set, :turn)
 
 # In-game-object mocks
-World = Struct.new(:size, :circumference_km, :polar_radius)
+World = Struct.new(:circumference_km, :polar_radius)
 Unit = Struct.new(:type, :society, :work_hours, :velocity_kmh)
 
 # Instance mocks
-exoplanet = World.new(160, 40000, 6356)
+exoplanet = World.new(40000, 6356)
 
 # DD 4.3.2.
-head = Unit.new("Head", nil, 14, 120)
+head = Unit.new("Head", "Headsociety", 14, 120)
 
-os << "RESULTS"
+os << "# RESULTS"
 
 os << "DD 4.1.1. - Time-limit/moves required for equatorial sectors"
 sectors = spec[:sector_grid]*18
@@ -75,20 +94,21 @@ turns_max = spec[:time_max_s]/spec[:time_turn_len_s]
 os << "For a #{spec[:time_max_s]/60} minute game, #{turns_max*spec[:time_days_per_turn]} in-world days." 
 os << "Maximum number of turns #{turns_max}."
 
-throw "RulesetLimitsError - head-unit travel lim @ limits[:sectors_visited_min]." unless travel_time_min_turns < turns_max 
-os << "✓ DD 4.1.1 & DD 4.9 PASS." 
-os << "_"
+os << "# LIMITS TESTS"
+throw "RulesetLimitsError - head-unit travel fails limits[:sectors_visited_min]." unless (travel_time_min_turns < turns_max) 
+os << "✓ DD 4.1.1 & DD 4.9 PASS."
 
+#Sanity score
 #pseudo game logic to mock resultant gameplay
 #f.ex. mock_gameplay(buildingRandomizer)
 #      Society.new(players_head, building)
 
-#ESTIMATE SCORE:
-#  Unit travel score
-#  memory & cpu req
-#  rule sanity score
-#  game_winnable
+os << "# RULE SCORE"
+unit_travel = (turns_max/travel_time_min_turns).floor
+req_mem = req_cpu = 20 * 0
+rule_sanity = 10 * 0
+game_winnable_bool = 20 * 0
+os << (unit_travel + req_mem + req_cpu + rule_sanity + game_winnable_bool).to_s + " % / 100 %"
 
 # Print results
 puts os
-
