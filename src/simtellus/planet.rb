5a590255a30cb60aa898c9f85f863c0f02f691c0
Casimir Pohjanraito <casimir.pohjanraito@gmail.com>
2023-11-15 19:17:30 +0200

    Separate modules Planet and Tests.

--- src/simtellus/planet.rb
+++ src/simtellus/planet.rb
@@ -1,14 +1,11 @@
-# encoding: utf-8
-
 module Planet
-
   # Collection of modules to approximate planetary energy fluxes in ISO units.
   # TODO test suite
   # TODO render svg graph for year 2017 and 2017-2037 year periods
 
   # Physical Constants
   G = 9.80665 # gravitational acceleration at sea-level, m/s²
-  
+
   # into space
   Energy_Exradiation_IR_Total = 239.9 # Wm²
 
@@ -18,22 +15,23 @@ module Planet
   # period_len: period length, from 0
   # y_max, y_min: min/max to extrapolate sine -1..1
   # returns extrapolated sinus y value for current position x
-  def self.sinus2range posi, period_len, y_max, y_min
-    posi = posi.to_f / period_len.to_f * (2*Math::PI);
-    val = Math.sin( posi.to_f );
+  def self.sinus2range(posi, period_len, y_max, y_min)
+    posi = posi.to_f / period_len * (2 * Math::PI)
+    val = Math.sin(posi.to_f)
     dist = y_max - y_min
-    y_min + (1+val)/2 * dist
+    y_min + (1 + val) / 2 * dist
   end
 
   # Net energy available on surface
   # Wm²
-  def self.energy_transmitted yearday, lat
-    Atmos::Solar_Transmission * insolation_multiplier(yearday, lat)
+  def self.energy_transmitted(yearday, lat)
+    solar_irradiance_wm2(yearday) *
+      insolation_multiplier(yearday, lat)
   end
 
   module EMField
     # The actual direct solar irradiance at the top of the atmosphere fluctuates by
-    # about 6.9% during a year (from 1.412 kW/m² in early January to 1.321 kW/m² in 
+    # about 6.9% during a year (from 1.412 kW/m² in early January to 1.321 kW/m² in
     # early July) due to the Earth's varying distance from the Sun, and typically
     # by much less than 0.1% from day to day.
 
@@ -42,83 +40,72 @@ module Planet
 
     # yearday is the day of the year 1..365
     # returns multiplier 0..2
-    def solar_cycle yearday
+    def solar_cycle(_yearday)
       # Schwabe cycle of 10.66 a, Gleisberg 88 a, Devries 208 a, Eddy 1000 a
-      # last maxima july 1990, feb 2001 -> next maxima 2011 
+      # last maxima july 1990, feb 2001 -> next maxima 2011
       # Solar_Constant + sinus2range( days % 10.66, 10.66, -Solar_Constant_Range,
       # Solar_Constant_Range )
       # todo normalize to nearest maxima
-      #p "EMField.solar_cycle is static sunspot mock."
+      # p "EMField.solar_cycle is static sunspot mock."
       0.999
     end
 
     # returns what?
-    def space_weather yearday
+    def space_weather(_yearday)
       # solar wind
       # magnetic storms
       1
     end
 
-    # lat is the latitude -90..0..90, negative latitude denoting southern hemisphere
     # arg yearday 1..365
     # returns W/m²
-    def solar_irradiance_wm2 yearday, lat
-      Solar_Constant * 
-        solar_cycle(yearday) * 
-        orbital_effect(yearday) *
-        axis_tilt_daylength_effect(lat, yearday) *
-        incident_angle_effect(lat, yearday)
+    def solar_irradiance_wm2(yearday)
+      EMField::Solar_Constant *
+        solar_cycle(yearday) *
+        orbital_effect(yearday)
     end
 
-
-    def insolation_multiplier yearday, lat
-      # https://en.wikipedia.org/wiki/File:Insolation.png
-      solar_irradiance_wm2(yearday, lat) / Atmos::Solar_Transmission
-    end
-
-    def uv_influx yearday, long, lat, elevation
-      p "EMField.uv multiplier mock is static"
+    def uv_influx(yearday, long, lat, elevation)
+      p 'EMField.uv multiplier mock is static'
       uv_multiplier = 1
       insolation(yearday, long, lat, elevation) * uv_multiplier
     end
-
   end
 
+  # Effects of orbital eccentricity and obliquity
+  # todo memoization
   module Orbit
+    require 'date'
+    # equinox on Mar 20th
+    EQUINOX_DAY_N = Date.civil(2020, 3, 20).yday
 
     #	Calculates the daylength (compared to 1 = 24h)
     #	arg day of year 1..365
     #	returns multiplier
-    def orbital_effect yearday
+    def orbital_effect(yearday)
       # Orbit eccentricity:
       # sine wave period of 365 d, modulating day length by 0..-7,66m..0..+7,66 minutes
-      # Orbit obliquity: 
+      # Orbit obliquity:
       # sine wave period of 182.5 d, modulates day length by 0..-9,87..0..+9,87 minutes
-      orb_ecce = sinus2range( yearday, 365, -7.66, 7.66 )
-      orb_obli = sinus2range( yearday, 182.5, -9.87, 9.87 )
-      (orb_ecce + orb_obli) / (24*60) + 1
+      orb_ecce = sinus2range(yearday, 365, -7.66, 7.66)
+      orb_obli = sinus2range(yearday, 182.5, -9.87, 9.87)
+      (orb_ecce + orb_obli) / (24 * 60) + 1
     end
 
-    # daylength, multiplier
+    # Daylength multiplier
+    # Earth’s current tilt angle is approximately 23.5 degrees.
+    # -> daylenght varies -> seasons, more sunshine in the summer
     #	lat 90..0..-90 - southern latitudes negative
     #	returns 0..1, 0.5 == 12h
-    def axis_tilt_daylength_effect lat, yearday
-      # Causes season
-      # Earth’s current tilt angle is approximately 23.5 degrees.
-      # -> daylenght varies -> more sunshine in the summer for north
+    def axis_tilt_daylength_effect(lat, yearday)
       # Hat-top cycle ver 1:
       # sine wave period of 365d, modulates day lenght by latitudal distance multi
       # note: offset results by substracting from yearday
-      require 'date'
 
-      # equinox on Mar 20th 
-      days_to_solar_equinox_from_new_years = Date.civil(2020, 03, 20).yday
-
-      # Sinus Offset
+      # offset for sinus func
       # sinus peaks at first quarter of the scale, summer peaks at halfway (NH) or
       # end (SH) of the scale
-      # offset for sinus func (max in jun)
-      yearday_adjusted = (yearday - days_to_solar_equinox_from_new_years) % 365
+      yearday_adjusted = (yearday - EQUINOX_DAY_N) % 365
 
       # Determine start of year for latitude - days begin to longen (start of sinus)
       hemisphere_multi = (lat > 0 ? 0 : 1)
@@ -127,21 +114,21 @@ module Planet
 
       # Determine daylength extremes
       lat_arctic_circle = 66.533 # 66 deg 32 min
-      lat_daylen_offset = ( lat.to_f / lat_arctic_circle ).abs.floor # 0..1
+      lat_daylen_offset = (lat.to_f / lat_arctic_circle).abs.floor # 0..1
       day_max_len = 12 + 12 * lat_daylen_offset # 12..24
       day_min_len = 12 - 12 * lat_daylen_offset # 0..12
 
-      sinus2range(yearday_adjusted, 365, day_max_len, day_min_len ) / 24
+      sinus2range(yearday_adjusted, 365, day_max_len, day_min_len) / 24
     end
 
-    def incident_angle_effect lat, yearday
-      1 - sinus2range( lat.abs.to_f, 360, 0.9, 0 ) # ruff approx
+    # Returns 0..1
+    def incident_angle_effect(lat, _yearday)
+      0.5 - (lat.abs / 90)
+      # 1 - sinus2range( lat.abs.to_f, 360, 0.9, 0 ) # ruff approx
     end
-
   end
 
   module Atmos
-
     # Primitive troposphere model
 
     M_air = 0.0289644 # air molar mass, kg/mol
@@ -153,10 +140,10 @@ module Planet
     # Planetary sphere surface flux, Wm²
     Solar_Influx            = 340.4
     Solar_Influx_Reflected  = 99.9 # into space
-    Solar_Transmission      = 240.5 # source ?
-    Solar_Trans_Twilight    = 10 # source ?  
-    Reflection_Atmos        = 77.0 
-    Absorption_Influx       = 77.1 
+    Solar_Transmission_W    = 240.5 # source ?
+    Solar_Trans_Twilight    = 10 # source ?
+    Reflection_Atmos        = 77.0
+    Absorption_Influx       = 77.1
     Absorption_Geosp_IR     = 358.2
 
     # Outgoing Longwave Radiation
@@ -164,73 +151,80 @@ module Planet
     Exradiation_IR_Clouds   = 29.9
     Exradiation_IR_Geosp    = 40.1
 
+    # lat is the latitude -90..0..90, negative latitude denoting southern hemisphere
+    # returns 0..1
+    def insolation_multiplier(yearday, lat)
+      # https://en.wikipedia.org/wiki/File:Insolation.png
+      # weather *
+      # extreme_weather *
+      # biome *
+      incident_angle_effect(lat, yearday) *
+        axis_tilt_daylength_effect(lat, yearday)
+    end
+
     # Returns average C deg
-    def temp yearday, lat, elev, biome
+    def temp(_yearday, _lat, _elev, _biome)
       # Average temperature is 15 C according to NASA
       # Low Vostok -89.2 C, High Lut Desert 70.7 C
       # -> +-60C norm, if weather extreme, multiply by 1.25
       # -> +-80C max
       # elev loss
       # biome
-      p "Atmos.temp is wip @ 15 C"
+      p 'Atmos.temp is wip @ 15 C'
       15
     end
 
     # Air pressure at elevation
     # returns hectopascals, (P_air units is hPa).
-    def pressure yearday, lat, elev
+    def pressure(yearday, lat, elev)
       # from https://www.omnicalculator.com/physics/air-pressure-at-altitude
       # theta temp should be cald'd
       kelvin_zero = 273.15
-      t_K = temp( yearday, lat, elev, 1) + kelvin_zero 
-      P_air * Math.exp( -G*elev*M_air / (R_air * t_K) )
+      t_K = temp(yearday, lat, elev, 1) + kelvin_zero
+      P_air * Math.exp(-G * elev * M_air / (R_air * t_K))
     end
 
     # Returns mm/sqkm
-    def rain yearday, lat, elev, biome
+    def rain(_yearday, _lat, _elev, _biome)
       # biome adjust
       # http://earthobservatory.nasa.gov/Experiments/Biome/
       # dist from equator decreases
       # http://www-das.uwyo.edu/~geerts/cwx/notes/chap10/global_precip.gif
       # 10% bonus for southern hemi
       # low pressure gives rain
-      p "Atmos.rain is static mock."
+      p 'Atmos.rain is static mock.'
       1
     end
 
     def weather_extreme_bool
       false
     end
-
   end
 
   module Geosphere
- 
     # source NP-2010-05-265-LaRC
     # Wm²
     # into geosphere
     Net_Absorption = 0.6 # Wm² absorbed
     Solar_Influx_Absorption   = 163.3 # solar influx absorbed
     Solar_Influx_Reflected    = 22.9
-    Atmospheric_Backradiation = 340.3  
+    Atmospheric_Backradiation = 340.3
 
     # into atmos
     Exradiation_Surface       = 398.2
     Exconvection_Exconduction = 18.4
     Ex_Latent_Heat            = 86.4
-
   end
 
   module Biome
-
     # Requires an world conf with biomes (world.yml)
     # self.biomes = _world.Universe.Earth.biomes
 
     # Returns a multiplier for args biome and type of adjustment
-    def self.biome_adjust biome_code, type
-      if type == "insolation"
-      elsif type == "temp"
-      elsif type == "rain"
+    def biome_multiplier biome_code, type
+      if type == 'insolation'
+      elsif type == 'temp'
+      elsif type == 'rain'
         # sea averages near 12C
       end
     end
@@ -238,167 +232,16 @@ module Planet
     # Returns biome adjustment object.
     #	Effect multipliers for insolation due to weather,
     #	temp
-    def self.get_biome_adjust
-    end
+    def biome_adjust_obj; end
 
     def fauna
       # TODO
     end
   end
 
-  module Tests
-
-    # Run tests with '$ ruby planet.rb'.
-    # run_tests method executes all test_* methods.
-
-    def test_sinus2range
-      [[0, 1], [0.75, 3], [1.5, 1], [2.25, -1], [3, 1]].all? { |n|
-        _res = sinus2range(n[0], 3, 3, -1).round(3)
-        # p "s2r result: " + _res.to_s + " vs " + n[1].to_f.to_s
-        n[1].to_f == _res
-      }
-    end
-
-    def test_energy_transmitted
-      cases = [
-        [:non_polar_latitudes,
-         { :max => EMField::Solar_Constant * EMField::Solar_Constant_Range,
-           :latitudes => (0..66).step(6).to_a,
-           :dayrange => (1..365).step(5).to_a }],
-        [:antarctic,
-         { :max => Atmos::Solar_Trans_Twilight * 2,
-           :latitudes => (-90..-69).step(4).to_a,
-           :dayrange => (172..202).step(5).to_a }],
-        [:arctic,
-         { :max => Atmos::Solar_Trans_Twilight * 2,
-           :latitudes => (68..90).step(4).to_a,
-           :dayrange => (335..365).step(5).to_a }]
-      ]
-
-      cases.all? { |c, params|
-        params[:latitudes].all? { |lat|
-          params[:dayrange].all? { |d|
-            _r = energy_transmitted(d, lat)
-            p _r, lat, d, c unless _r < params[:max]
-            _r.class == Float && _r < params[:max]
-          }
-        }
-      }
-    end
-
-    def test_solar_cycle
-      _res = solar_cycle(rand * 365)
-      _res.class == Float && _res <= 2 && _res >= 0
-    end
-
-    def test_space_weather
-      p "make test for space_weather -method"
-      true # no test
-    end
-
-    def test_orbital_effect
-      # Should return day length multiplier 0..1,
-      # - so case "10h/day" --> solar irradiance multiplier ~0.84 (shines most of day)
-      _res = orbital_effect( rand * 365 )
-      _res.class == Float && _res >= 1420.0/1440 && _res <= 1460.0/1440
-    end
-
-    def test_axis_tilt_daylength_effect
-      constraints = [ 
-        # < 1 h
-        [:polar_night, 1.to_f/24, [
-          # North pole, September 24 - December 22
-          { lat: 90, start: 268, end: 357, },
-          # South pole, March 22 - June 20
-          { lat: -90, start: 82, end: 170 }
-        ]],
-        # > 23 h
-        [:polar_day, 23,[
-          # North pole, March 22 - September 21
-          { lat: 90, start: 82, end: 265 },
-          # South pole, December 22 - December 30
-          { lat: -90, start: 357, end: 264 },
-          # South pole, January 1 - March 21
-          { lat: -90, start: 1, end: 82 }
-        ]],
-        # ~12 h
-        [:equatorial_day, 11,
-          # Equator, January 1 - June 30
-         [{ lat: 0, start: 1, end: 180 }]
-        ]
-      ]
-
-      constraints.all? do |env, tlen, params|
-        params.each { |par|
-          (par[:start]...par[:end]).each { |yearday|
-              _res = axis_tilt_daylength_effect(par[:lat], yearday)
-              _res.send((env == :polar_night ? :< : :>), tlen)
-          }
-        }
-      end
-    end
-
-    def test_incident_angle_effect
-      p "Incident angle effect test is mock."
-      _res = incident_angle_effect( -90 + rand*180, 100000 )
-      _res.class == Float && _res >= 0 && _res <= 1
-    end
-
-    def test_solar_irradiance
-      # the average incoming solar radiation, considering incident angle and half
-      # the planet doesn't receive radiation, is 1/4 the solar constant ~340 W/m²
-      # the daily average irradiance for Earth is approx 250 W/m2, 6 kWh/m2/d
-      # source?
-      _res = solar_irradiance_wm2( rand * 365, rand * 90 )
-      p "solar irradiance: #{_res} wm2"
-      _res.class == Float && _res >= 0 && _res < Planet::EMField::Solar_Constant * 1.2
-    end
-
-    def test_insolation_multiplier
-      _res = insolation_multiplier( rand * 365, -90 + rand*180)
-      p "insolation multiplier: #{_res}"
-      _res.class == Float && _res >= 0 && _res < Planet::EMField::Solar_Constant * 0.75
-    end
-
-    # def test_uv_influx yearday, long, lat, elevation
-    # def test_orbital_effect yearday
-    # def test_atmospheric_effect
-    # def test_atmospheric_tide yearday
-    def test_pressure
-      _res = pressure( rand * 365, -90 + rand*180, rand*100 )
-      p "Airpressure: #{_res} hPA."
-      ( # hPa @ Typhoon Tip, Pacific 12.10.1979.
-       _res > 870 &&
-       # hPa @ Tosontsengel, Mongolia 19.12.2001.
-      _res < 1085
-      )
-    end
-
-    # def test_temp yearday, lat, elev, biome
-    # def test_rain yearday, lat, elev, biome
-    # def test_weather_extremes yearday
-    # def test_biome_adjust biome_code, type
-    # def test_fauna
-
-    # def render_line_graph_for_period year-range
-
-    private 
-    def run_tests
-      puts "Testrun for " + name
-      res = Tests.instance_methods.all?{|m|
-        _r = send(m)
-        puts ">> #{m.to_s} pass: #{_r.to_s}"
-        _r 
-      } 
-      puts (res ? "Super! Tests pass." : "Fail!!! Test(s) not passing.")
-    end
-
-  end
-
-  extend EMField, Orbit, Atmos, Biome, Geosphere, Tests
-  #require 'pry'#binding.pry
-  run_tests
-
+  extend Geosphere
+  extend Biome
+  extend Atmos
+  extend Orbit
+  extend EMField
 end
-
-include Planet
