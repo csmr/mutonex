bd3338e2c76bb7e831430743f26d580642902ffe
Casimir <csmr@users.noreply.github.com>
2024-10-21 23:40:32 +0300

    Update build-init.sh

--- src/build-init.sh
+++ src/build-init.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Set absolute base paths
+DEV_TGT=$(mktemp -d --tmpdir mutonex.XXXXXX)  # mktemp for secure temp dir
+#PROD_TGT="/opt/mutonex"
 
-# Set absolute base paths
-DEV_TGT=$(mktemp -d --tmpdir mutonex-dev-XXXXXX)  # mktemp for secure temp dir
-PROD_TGT="/opt/mutonex"
-
-# Setup base paths
 BASE_DIR="$DEV_TGT/build"
 RUNTIME_DIR="$DEV_TGT/build"
 DATA_HOME="$DEV_TGT/data"
 
-if [ ! -d "$DEV_TGT" ]; then
-  mkdir $DEV_TGT
-  mkdir $BASE_DIR
-  mkdir $DATA_HOME
+# Create directories if they do not exist
+mkdir -p "$BASE_DIR"
+mkdir -p "$DATA_HOME"
+mkdir -p "$PROD_TGT"
+#sudo chown $USER:$USER "$PROD_TGT"  # Ensure the user owns the directory
+
+# Install Deno
+if ! command -v deno &> /dev/null; then
+  curl -fsSL https://deno.land/x/install/install.sh | sh
 fi
