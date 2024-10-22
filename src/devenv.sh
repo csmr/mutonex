b984c5272ff3420c7c4f5fcaa19aa3744f5e1010
Casimir Pohjanraito <casimir.pohjanraito@gmail.com>
2024-10-22 22:37:58 +0300

    Update devenv runner

--- src/devenv.sh
+++ src/devenv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
 if ! command -v docker-compose &> /dev/null; then
   echo "sudo apt install docker-compose"
+  echo "sudo usermod -aG docker [username]"
+  echo "log out and back in, retry"
   exit 2
 fi
 
