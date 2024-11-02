70341e6a19d28afabaae5345592cc9c5a179d985
Casimir <csmr@users.noreply.github.com>
2024-11-02 18:15:30 +0200

    Update devenv.sh

--- src/devenv.sh
+++ src/devenv.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if ! command -v docker-compose &> /dev/null; then
+  echo "no docker-compose, on debian try:"
   echo "sudo apt install docker-compose"
   echo "sudo usermod -aG docker [username]"
   echo "log out and back in, retry"
-  exit 2
+  exit 1
 fi
 
 echo "мμτοηεχ δεv εηv ιηιτ"
