f2e7ac8c7d3fb9272285e6f2d18e738934b0ab4b
Casimir Pohjanraito <casimir.pohjanraito@gmail.com>
2025-11-17 22:30:07 +0200

    Update container configuration for db and webserver

--- src/scripts/init-dotenv.sh
+++ src/scripts/init-dotenv.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+### Generates `.env` from template if none ###
+### - depends on `.env.template` and `.env.postgres`
+
 source ./scripts/app.config.sh
 
-### Generate .env if no `./src/.env` ###
 ENV_PATH="$BASE_DIR/.env"
 TPL_PATH="$ENV_PATH.template"
 CRED_PATH="$DATA_HOME/.env.postgres"
 
 if [ -f $ENV_PATH ]; then
   log "$ENV_PATH exists, exit."
-  exit 1
+  exit 0
 fi
 cat $TPL_PATH > $ENV_PATH 
 log "Created $ENV_PATH file."
 
 if [ ! -f $CRED_PATH ]; then
   log "No $CRED_PATH to append."
-  exit
+  exit 1
 fi
 cat $CRED_PATH >> $ENV_PATH
 log "Appended $CRED_PATH to $ENV_PATH."
