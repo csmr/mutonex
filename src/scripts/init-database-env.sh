84b7a30ada54444e7e117846d5baa41e4c0d0098
Casimir Pohjanraito <casimir.pohjanraito@gmail.com>
2025-11-17 23:33:25 +0200

    Update credential randomization

--- src/scripts/init-database-env.sh
+++ src/scripts/init-database-env.sh
@@ -8,9 +8,9 @@ PGDATA=$DATA_HOME/postgres
 
 # bash portable random gen
 generate_random() {
-    local now=$(printf '%(%s)T' -1)  # time
-    local b64=$(printf "%s%s%s" $RANDOM $RANDOM $RANDOM | base64)
-    echo "${b64:2:16}"  # bash string slicing, more portable than cut
+    local now=$(date +%s%N) # time
+    local b64=$(printf "%s%s%s%s" $RANDOM ${now:13:1+RANDOM%5} $RANDOM $RANDOM | base64)
+    echo "${b64:2:17+RANDOM%3}" # bash string slicing, more portable than cut
 }
 
 if [ ! -f $ENV_PATH ]; then
@@ -22,7 +22,7 @@ if [ ! -f $ENV_PATH ]; then
     log "Generating $ENV_PATH..."
     cat > $ENV_PATH << EOL
 POSTGRES_USER=muto_user_$(generate_random)
-POSTGRES_PASSWORD=muto_pass_$(generate_random)
+POSTGRES_PASSWORD=$(generate_random)$(generate_random)
 POSTGRES_DB=muto_db_$(generate_random)
 EOL
 
