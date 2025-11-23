df4dd26e809597724adc068f2814d4b380fe9b10
Casimir <csmr@users.noreply.github.com>
2025-11-23 23:41:47 +0200

    Update STATIC_ROOT_DIR in start-webserver.sh

--- src/webserver/start-webserver.sh
+++ src/webserver/start-webserver.sh
@@ -17,6 +17,7 @@ deno run --allow-read --allow-write ./scripts/make-credits.js
 
 # Define and export the single source of truth for the static assets path
 export STATIC_ROOT_DIR="/app/dist"
+log "Set STATIC_ROOT_DIR to $STATIC_ROOT_DIR"
 
 # Run webserver
 deno run --allow-net --allow-env --allow-read=$STATIC_ROOT_DIR webserver/app.ts &
