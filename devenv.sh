9d8cac96aa67db87b8e4e31865ef6e4554a5bc6c
Casimir <csmr@users.noreply.github.com>
2026-08-22 16:11:22 +0300

    Clean up deps setup script

--- devenv.sh
+++ devenv.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-# Mutonex Root Orchestrator
-if command -v docker >/dev/null 2>&1 && docker compose version >/dev/null 2>&1; then
-  DOCKER_CMD="docker compose"
-elif command -v docker-compose >/dev/null 2>&1; then
-  DOCKER_CMD="docker-compose"
-else
-  echo "Docker Compose not found."
-fi
-if [ -n "$DOCKER_CMD" ]; then
-  echo "мμτοηεχ δεv εηv ιηιτ"
-  source ./scripts/app.config.sh
-  ./scripts/init-database-env.sh
-  ./scripts/init-dotenv.sh
-  mkdir -p "$RUNTIME_DIR" "$WEB_PATH"
-  git shortlog -n -s > "$RUNTIME_DIR/CONTRIBS"
-  touch "$WEB_PATH/index.html"
-  cd infra && $DOCKER_CMD --env-file "$DOTENV_PATH" up
-fi
+# Mutonex dev-env generator
+
+# runtimes in preference order.
+COMPOSERS=(
+  "podman compose"
+  "podman-compose"
+  "docker compose"
+  "docker-compose"
+)
+
+COMPOSE=()
+for c in "${COMPOSERS[@]}"; do
+  $c version >/dev/null 2>&1 && COMPOSE=($c) && break
+done
+
+[ -n "$COMPOSE" ] || {
+  echo "Podman or Docker cont compose not found, exit."
+  exit 0
+}
+
+echo "мμτοηεχ δεv εηv ιηιτ"
+source ./scripts/app.config.sh
+./scripts/init-database-env.sh
+./scripts/init-dotenv.sh
+mkdir -p "$RUNTIME_DIR" "$WEB_PATH"
+touch "$WEB_PATH/index.html"
+cd infra && "${COMPOSE[@]}" --env-file "$DOTENV_PATH" up
